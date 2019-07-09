a61d9dd3d3f5fc67d9b088fb96e84f5d979e0622
Dawid Stoga PGS <dstoga@pgs-soft.com>
2019-07-09 14:09:52 +0200

    Unit 2 lesson 1

---
+++ Unit2/Lesson1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using Akka.Actor;
+using Akka.Configuration;
+using ChartApp.Actors;
+
+namespace ChartApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// ActorSystem we'll be using to publish data to charts
+        /// and subscribe from performance counters
+        /// </summary>
+        public static ActorSystem ChartActors;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var configuration = ConfigurationFactory.Load();
+
+            ChartActors = ActorSystem.Create("ChartActors");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
