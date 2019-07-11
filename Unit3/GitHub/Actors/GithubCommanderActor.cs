a7561d90b1f8b6966051e6d1d080d13e0b70822f
Dawid Stoga PGS <dstoga@pgs-soft.com>
2019-07-11 14:01:25 +0200

    Unit3 - lesson 3

--- Unit3/GitHub/Actors/GithubCommanderActor.cs
+++ Unit3/GitHub/Actors/GithubCommanderActor.cs
@@ -66,7 +66,8 @@ namespace GithubActors.Actors
         private void BecomeAsking()
         {
             _canAcceptJobSender = Sender;
-            pendingJobReplies = 3; //the number of routees
+            pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees())
+                .Result.Members.Count();
             Become(Asking);
         }
         private void Asking()
@@ -107,21 +108,30 @@ namespace GithubActors.Actors
 
         protected override void PreStart()
         {
-            // create three GithubCoordinatorActor instances
-            var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "1");
-            var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "2");
-            var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "3");
 
-            // create a broadcast router who will ask all of them 
-            // if they're available for work
+            // create a broadcast router who will ask all 
+            // of them if they're available for work
             _coordinator =
-                Context.ActorOf(Props.Empty.WithRouter(
-                    new BroadcastGroup(ActorPaths.GithubCoordinatorActor.Path + "1",
-                    ActorPaths.GithubCoordinatorActor.Path + "2",
-                    ActorPaths.GithubCoordinatorActor.Path + "3")));
+                Context.ActorOf(Props.Create(() => new GithubCoordinatorActor())
+                  .WithRouter(FromConfig.Instance),
+                  ActorPaths.GithubCoordinatorActor.Name);
+            base.PreStart();
+
+            //// create three GithubCoordinatorActor instances
+            //var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
+            //    ActorPaths.GithubCoordinatorActor.Name + "1");
+            //var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
+            //    ActorPaths.GithubCoordinatorActor.Name + "2");
+            //var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
+            //    ActorPaths.GithubCoordinatorActor.Name + "3");
+
+            //// create a broadcast router who will ask all of them 
+            //// if they're available for work
+            //_coordinator =
+            //    Context.ActorOf(Props.Empty.WithRouter(
+            //        new BroadcastGroup(ActorPaths.GithubCoordinatorActor.Path + "1",
+            //        ActorPaths.GithubCoordinatorActor.Path + "2",
+            //        ActorPaths.GithubCoordinatorActor.Path + "3")));
             base.PreStart();
         }
 
