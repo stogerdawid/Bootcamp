6d681486c86a85a7156eaedd0e6218e36979cd65
Dawid Stoga PGS <dstoga@pgs-soft.com>
2019-07-11 15:09:08 +0200

    Unit3 -last lesson

--- Unit3/GitHub/Actors/GithubCommanderActor.cs
+++ Unit3/GitHub/Actors/GithubCommanderActor.cs
@@ -46,6 +46,7 @@ namespace GithubActors.Actors
         private int pendingJobReplies;
         private IActorRef _coordinator;
         private IActorRef _canAcceptJobSender;
+        private RepoKey _repoJob;
 
         public IStash Stash { get; set; }
 
@@ -59,6 +60,7 @@ namespace GithubActors.Actors
             Receive<CanAcceptJob>(job =>
             {
                 _coordinator.Tell(job);
+                _repoJob = job.Repo;
                 BecomeAsking();
             });
         }
@@ -69,7 +71,10 @@ namespace GithubActors.Actors
             pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees())
                 .Result.Members.Count();
             Become(Asking);
+            // send ourselves a ReceiveTimeout message if no message within 3 seconds
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(3));
         }
+
         private void Asking()
         {
             // stash any subsequent requests
@@ -98,12 +103,18 @@ namespace GithubActors.Actors
 
                 BecomeReady();
             });
+            Receive<ReceiveTimeout>(timeout =>
+            {
+                _canAcceptJobSender.Tell(new UnableToAcceptJob(_repoJob));
+                BecomeReady();
+            });
         }
 
         private void BecomeReady()
         {
             Become(Ready);
             Stash.UnstashAll();
+            Context.SetReceiveTimeout(null);
         }
 
         protected override void PreStart()
